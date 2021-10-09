15d873016085836b20288282ba5fa9c3936c9bf3
Zwx-tech <71787137+Zwx-tech@users.noreply.github.com>
2021-10-09 14:02:44 +0200

    movement
    v0.1

--- Test-Game/Assets/Scripts/PlayerMovement.cs
+++ Test-Game/Assets/Scripts/PlayerMovement.cs
@@ -8,10 +8,13 @@ public class PlayerMovement : MonoBehaviour
 {
 
     public Rigidbody2D rb;
-    public float max_speed;
-
-    public Vector2 veloctiy;
-    
+    // walking
+    public float step;
+    //jump
+    public bool on_ground;
+    public float jump_height;
+    // wall jump
+    public int on_wall=0; // between -1 and 1
     // Start is called before the first frame update
     void Start()
     {
@@ -23,13 +26,43 @@ public class PlayerMovement : MonoBehaviour
     void FixedUpdate()
     {
         // check inputs
-        
+        CheckInputs();
+        // check if player is on the ground(7)
+        CheckIfGrounded();
         // move player
-        rb.MovePosition(rb.position + veloctiy * Time.fixedDeltaTime);
+    
     }
 
     void CheckInputs()
     {
         // magick
+        float dirx = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(dirx*step, rb.velocity.y);
+    } 
+
+    void CheckIfGrounded()
+    { 
+        on_ground = false;
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.TransformDirection(direction: Vector3.down), 0.8f, LayerMask.GetMask("Ground"));
+        if (hit)
+        {
+            on_ground = true;
+        }
+        
+        Jump();
+    }
+
+    void Jump()
+    {
+        // normal jump
+        if (Input.GetButton("Jump") && on_ground)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jump_height);
+        }
+        // wall jump
+        if (Input.GetButton("Jump") && on_wall != 0)
+        {
+            
+        }
     }
 }
